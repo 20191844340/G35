4322f0d8f857045e976c12426131a017685976a2
20191844340 <56434090+20191844340@users.noreply.github.com>
2019-10-21 16:09:33 +0800

    3.1

---
+++ 3.1/111.cpp
@@ -0,0 +1,18 @@
+// 111.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include<math.h>
+ 
+int main()
+{
+	float p,r,n;
+	r=0.07;
+	n=10;
+	p=pow(1+r,n);
+	printf("p=%f\n",p);
+	return 0;
+}
+
+
