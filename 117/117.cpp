794fab65b0dc27b0292d59ad883000144465bb80
20191844301 <56433542+20191844301@users.noreply.github.com>
2019-10-22 15:58:23 +0800

    第三章

---
+++ 117/117.cpp
@@ -0,0 +1,14 @@
+// 117.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{int i,j;
+for (i=1;i<=10; i++)
+{for (j=1;j<=10;j++)
+	printf("*");
+printf("\n");}
+	return 0;
+}
+
